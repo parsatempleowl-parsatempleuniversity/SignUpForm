887f27ca37295826162eee72a9864b96ec1e19f7
Parsa Temple Owl <tug81042@temple.edu>
2020-02-12 15:13:02 -0500

    added the string variables for the string literals as part of onSetClickListener for the Save button

--- app/src/main/java/edu/temple/FormActivity.java
+++ app/src/main/java/edu/temple/FormActivity.java
@@ -32,20 +32,26 @@ public class FormActivity extends AppCompatActivity {
                 String password = Password.getText().toString();
                 String passowrdConfirmation = PasswordConfirmation.getText().toString();
 
-                if(name.equals("")) {
-                    Toast.makeText(getApplicationContext(), "Please enter name", Toast.LENGTH_LONG).show();
+                String name1 = "Name";
+                String email1 = "E-mail";
+                String password1 = "Password";
+                String passwordConfirmation1 = "Password Confirmation";
+                String emptyString = "";
+
+                if(name.equals(emptyString) || (name.equals(name1))) {
+                    Toast.makeText(getApplicationContext(), "Please enter your name", Toast.LENGTH_LONG).show();
                 }
-                else if(email.equals("")) {
-                    Toast.makeText(getApplicationContext(), "Please enter email", Toast.LENGTH_LONG).show();
+                else if(email.equals(emptyString) || (email.equals(email1))) {
+                    Toast.makeText(getApplicationContext(), "Please enter your email", Toast.LENGTH_LONG).show();
                 }
-                else if(password.equals("")) {
-                    Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_LONG).show();
+                else if(password.equals(emptyString) || (password.equals(password1))) {
+                    Toast.makeText(getApplicationContext(), "Please enter your password", Toast.LENGTH_LONG).show();
                 }
-                else if(passowrdConfirmation.equals("")) {
-                    Toast.makeText(getApplicationContext(), "Please enter password again", Toast.LENGTH_LONG).show();
+                else if(passowrdConfirmation.equals(emptyString) || passowrdConfirmation.equals(passwordConfirmation1)) {
+                    Toast.makeText(getApplicationContext(), "Please enter your password again", Toast.LENGTH_LONG).show();
                 }
                 else if(!password.equals(passowrdConfirmation)) {
-                    Toast.makeText(getApplicationContext(), "Please enter the same password for password and password confirmation", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Please enter the same password for Password and Password Confirmation", Toast.LENGTH_LONG).show();
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "Welcome, " + name + ", to the SignUpFormApp", Toast.LENGTH_LONG).show();
