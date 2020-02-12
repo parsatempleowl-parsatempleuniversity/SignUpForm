4a25f6b2e906542e1235b5c964e0a2bb63324683
Parsa Temple Owl <tug81042@temple.edu>
2020-02-12 13:48:59 -0500

    Created an object for Name, Email, Password, Password Confirmation, and Save

--- app/src/main/java/edu/temple/FormActivity.java
+++ app/src/main/java/edu/temple/FormActivity.java
@@ -3,12 +3,23 @@ package edu.temple;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText Name = findViewById(R.id.Name);
+        EditText Email = findViewById(R.id.Email);
+        EditText Password = findViewById(R.id.Password);
+        EditText PasswordConfirmation = findViewById(R.id.PasswordConfirmation);
+        Button Save = findViewById(R.id.Save);
+
     }
 }
